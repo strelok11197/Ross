b477fb87f29396edd6541be6c7741ef3bcd77627
Strelok11197 <dima_vibe_97@mail.ru>
2019-10-19 22:14:45 +0300

    12

--- stradds.cpp
+++ stradds.cpp
@@ -1,8 +1,8 @@
 #include "ros/ros.h"
 #include "my_service/AddStr.h"
 
-bool add(service::AddStr::Request &req,
-	service::AddStr::Response &res)
+bool add(my_service::AddStr::Request &req,
+	my_service::AddStr::Response &res)
 {
 	res.str3 = req.str1 + req.str2;
 	return true;
@@ -12,7 +12,7 @@ int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "adds");
 	ros::NodeHandle n;
-	ros::ServiceServer service = n.advertiseService(ros::this_node::get_Name(), adds);
+	ros::ServiceServer service = n.advertiseService(ros::this_node::get_Name(), add);
 	ros::spin();
 	return 0;
 }
